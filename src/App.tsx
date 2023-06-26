ccab356437f2d746466437e2560844366ad4a066
17u5h <102058870+17u5h@users.noreply.github.com>
2023-06-27 00:08:54 +0700

    some initial remarks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			сайт для поиска пропавших питомцев
+		</div>
+	);
 }
 
 export default App;
