af559ea54ea31151740d07cd96fadff9739f3eb7
17u5h <102058870+17u5h@users.noreply.github.com>
2023-07-03 19:07:14 +0700

    header main-page pet-page adjusted

---
+++ src/components/Header/CreateAdvButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UIDarkButton from "../UI/UIDarkButton";
+
+const CreateAdvButton = () => {
+	return (
+		<UIDarkButton>
+			Подать объявление о пропаже
+		</UIDarkButton>
+	);
+};
+
+export default CreateAdvButton;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
-import PetDescription from "../pages/PetDescription";
+import PetDescriptionPage from "../pages/PetDescriptionPage";
 import MainPage from "../pages/MainPage";
 import NotFound from "../pages/NotFound";
 import UserProfilePage from "../pages/UserProfilePage";
 
 const AppRoutes = () => {
-	const isAuth = true
+	const isAuth = true																							//useSelector
 
 	return (
 		<Routes>
 			<Route path="/" element={<MainPage />}></Route>
-			<Route path="pet/:id" element={<PetDescription />} />
+			<Route path="pet/:id" element={<PetDescriptionPage />} />
 
 			<Route element={<ProtectedRoute isAuth={isAuth} />}>
 				<Route path="profile" element={<UserProfilePage />}></Route>
